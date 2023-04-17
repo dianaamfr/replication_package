dada8dc906b783ebac1c7f449f83134e95bca7fc
Diana Freitas <diana.amfreitas@gmail.com>
2023-04-17 17:43:05 +0100

    Fix docker install script

--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -4,8 +4,8 @@ sudo apt-get install \
     ca-certificates -y \
     curl -y \
     gnupg -y
-sudo install -m 0755 -d /etc/apt/keyrings -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
