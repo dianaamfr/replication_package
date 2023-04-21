84b711b9774ffc6e80c13c7d4eda79c191908731
Diana Freitas <diana.amfreitas@gmail.com>
2023-04-21 17:47:38 +0100

    Add busy write generator

---
+++ src/main/java/com/dissertation/validation/BusyWriteGenerator.java
@@ -0,0 +1,85 @@
+package com.dissertation.validation;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dissertation.referencearchitecture.client.Client;
+import com.dissertation.utils.Address;
+import com.dissertation.utils.Utils;
+import com.dissertation.validation.logs.Log;
+import com.google.protobuf.ByteString;
+
+public class BusyWriteGenerator {
+    private final Client client;
+    private final List<String> keys;
+    private final ArrayDeque<Log> logs;
+
+    private static final String USAGE = "Usage: BusyWriteGenerator <regionPartitions:Int> <readPort:Int> <readIp:String> (<writePort:Int> <writeIp:String> <partition:Int>)+ <keys:String>";
+
+    public BusyWriteGenerator(Address readAddress, List<Address> writeAddresses, List<String> keys) {
+        this.client = new Client(readAddress, writeAddresses);
+        this.keys = keys;
+        this.logs = new ArrayDeque<>(Utils.MAX_LOGS);
+
+        if(Utils.LOGS) {
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                public void run() {
+                    client.shutdown();
+                    Utils.logToFile(logs, String.format("%s-%s", Utils.WRITE_CLIENT_ID, Utils.getCurrentRegion().toString()));
+                }
+            });
+        }
+    }
+
+    public static void main(String[] args) {
+        int regionPartitions = 0;
+        Address readAddress;
+        List<Address> writeAddresses = new ArrayList<>();
+        List<String> keys = new ArrayList<>();
+
+        if (args.length < 7) {
+            System.err.println(USAGE);
+            return;
+        }
+
+        try {
+            regionPartitions = Integer.parseInt(args[0]);
+            readAddress = new Address(Integer.parseInt(args[1]), args[2]);
+            int addressesEndIndex = regionPartitions * 3 + 3;
+            for (int i = 3; i < addressesEndIndex; i += 3) {
+                writeAddresses.add(new Address(Integer.parseInt(args[i]), args[i + 1], Integer.parseInt(args[i + 2])));
+            }
+            
+            if(args.length < addressesEndIndex + 1) {
+                System.err.println(USAGE);
+                return;
+            }
+
+            for (int i = addressesEndIndex; i < args.length; i ++) {
+                keys.add(args[i]);
+            }
+
+            BusyWriteGenerator writer = new BusyWriteGenerator(readAddress, writeAddresses, keys);
+            writer.run();
+        } catch (NumberFormatException e) {
+            System.err.println(USAGE);
+            return;
+        }
+    }
+
+    public void run() {
+        String key;
+        ByteString value;
+        long payload = System.currentTimeMillis();
+        long count = 0;
+
+        while (count < Long.MAX_VALUE) {
+            key = keys.get((int) (count % keys.size()));
+            value = Utils.byteStringFromString(String.valueOf(payload));
+            this.client.requestWrite(key, value);
+            count++;
+        }
+    }
+
+}
