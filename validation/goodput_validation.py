3c23734a8e05d7d7b52a0c19095a2cc78577ae46
Diana Freitas <diana.amfreitas@gmail.com>
2023-04-25 22:31:09 +0100

    Update goodput validation

--- validation/goodput_validation.py
+++ validation/goodput_validation.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 from utils import get_data
 import os
@@ -8,32 +9,23 @@ LOGS_DIR = PATH + '/logs/goodput'
 LOG_RESULT_FILE = PATH + '/results/goodput/logs.csv'
 LOG_STATS_FILE = PATH + '/results/goodput/validation.csv'
 
-def get_region_stats(df, region):
+def combine_logs(iteration_dir):
+    dest_file = open(LOG_RESULT_FILE ,'w+', newline='\n', encoding='utf-8')
+
+    df = get_data(LOGS_DIR + '/' + iteration_dir, 'readclient-eu-west-1')
+
     df_result = pd.DataFrame()
-    df_result['total_writes'] = df['totalWrites']
+    df_result['total_writes'] = df['totalVersions']
     df_result['total_bytes'] = df_result['total_writes'] * PAYLOAD_BYTES
     df_result['elapsed_time'] = df['time']
-    df_result['writes_per_second'] = (df_result['total_writes'] * PAYLOAD_BYTES) / df_result['elapsed_time']
+    df_result['writes_per_second'] = (1000 * df_result['total_writes']) / df_result['elapsed_time']
     df_result['bytes_per_second'] = df_result['total_bytes'] / df_result['elapsed_time']
-    df_result['region'] = region
-    return df_result
-
-def combine_logs():
-    dest_file = open(LOG_RESULT_FILE ,'w+', newline='\n', encoding='utf-8')
-  
-    df_us = get_data(LOGS_DIR, 'readclient-us-east-1')
-    df_eu = get_data(LOGS_DIR, 'readclient-eu-west-1')
-
-    df_result_us = get_region_stats(df_us, 'us-east-1')
-    df_result_eu = get_region_stats(df_eu, 'eu-west-1')
-
-    df_concat = pd.concat([df_result_eu, df_result_us], axis=0).reset_index(drop=True)
-    df_concat.to_csv(dest_file, index=False)
+    df_result.to_csv(dest_file, index=False)
 
 
 def get_stats():
     df = pd.read_csv(LOG_RESULT_FILE)
-    df_results = df.groupby('region')['bytes_per_second']
+    df_results = pd.DataFrame()
 
     df_results['99%'] = df.quantile(q=0.99)
     df_results['95%'] = df.quantile(q=0.95)
@@ -46,5 +38,5 @@ def get_stats():
 
 
 if __name__ == '__main__':
-    combine_logs()
+    combine_logs(sys.argv[1])
     get_stats()
