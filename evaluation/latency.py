ffc274d4dab6c380d951f372b6fecd05f8df698e
Diana Freitas <diana.amfreitas@gmail.com>
2023-05-16 22:00:29 +0100

    Fix univariate latency plots

--- evaluation/latency.py
+++ evaluation/latency.py
@@ -1,105 +1,110 @@
 import pandas as pd
 import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
-from utils import get_data
-import os
+import matplotlib.ticker as ticker
+from utils import PATH, CC_DIR, EC_DIR, LOCAL_REGION, DELAYS, PERCENTILES_FLOAT, PERCENTILES
+from utils import get_data, df_describe
 import math 
 
-PATH = os.path.dirname(os.path.abspath(__file__))
-LOGS_DIR = PATH + '/logs/latency'
+EC_LATENCY_PATH = PATH + '/logs/latency' + EC_DIR + '/d_'
+CC_LATENCY_PATH = PATH + '/logs/latency' + CC_DIR + '/d_'
 
-def latency_times(iteration_dir, region):
-    df = get_data(LOGS_DIR + '/' + iteration_dir, 'readclient-' + region)
-    
-    df_result = df.groupby('id').apply(lambda group: 
-        group.loc[group['logType'] == 'ROT_RESPONSE', 'time'].values[0] - 
-        group.loc[group['logType'] == 'ROT_REQUEST', 'time'].values[0]).reset_index(name='latency')
+RESULT_PATH = PATH + '/results/latency'
 
-    return df_result.tail(100).reset_index(drop=True)
+def latency_evaluation():
+    dfs = []
 
-def latency_boxplot(df_ev_latency, df_cc_latency):
-    percentiles = [50, 70, 95, 99]
-    latency_ev_summary = df_ev_latency['latency'].describe(percentiles=[p/100 for p in percentiles])
-    latency_cc_summary = df_cc_latency['latency'].describe(percentiles=[p/100 for p in percentiles])
+    for delay in DELAYS:
+        df_ev_latency = get_latency_times(EC_LATENCY_PATH, delay)
+        df_ev_latency['consistency'] = 'EC'
+        df_ev_latency['goodput'] = "{:.0f}".format(1000/delay)
+                                 
+        df_cc_latency = get_latency_times(CC_LATENCY_PATH, delay)
+        df_cc_latency['consistency'] = 'CC'
+        df_cc_latency['goodput'] = "{:.0f}".format(1000/delay)
 
-    # Latency distribution boxplot
-    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 7))
+        dfs.append(df_ev_latency)
+        dfs.append(df_cc_latency)
 
-    max_latency = max(max(df_ev_latency['latency']), max(df_cc_latency['latency'])) + 5
-    axs[0].set_yticks(range(0, max_latency, 5))
-    axs[1].set_yticks(range(0, max_latency, 5))
+        # Latency distribution table
+        latency_distribution_table(df_ev_latency, df_cc_latency, delay)
 
-    df_ev_latency.plot.box(y='latency', ax=axs[0], grid=True)
-    axs[0].set_title('Eventually Consistent Baseline EU')
+    # Latency distribution boxplots
+    df = pd.concat(dfs)
+    latency_boxplot(df, outliers=False, interval=5, fig_size=(15, 7))
+    latency_boxplot(df, outliers=True, interval=10, fig_size=(20, 7))
 
-    for p in percentiles:
-        axs[0].axhline(latency_ev_summary[f'{p}%'], linestyle='--', color='green', label=f'{p}%')
-        axs[1].axhline(latency_cc_summary[f'{p}%'], linestyle='--', color='green', label=f'{p}%')
+    # Latency barplot
+    latency_barplot(df)
 
-    axs[0].axhline(latency_ev_summary['mean'], linestyle='-', color='blue', label='mean')
-    axs[0].axhline(latency_ev_summary['min'], linestyle=':', color='gray', label='min')
-    axs[0].axhline(latency_ev_summary['max'], linestyle=':', color='gray', label='max')
-    axs[0].legend()
-    axs[0].set_ylabel('Latency (ms)')
+    # Latency histogram
+    # latency_histogram(df)
 
-    axs[1].axhline(latency_ev_summary['mean'], linestyle='-', color='blue', label='mean')
-    axs[1].axhline(latency_ev_summary['min'], linestyle=':', color='gray', label='min')
-    axs[1].axhline(latency_ev_summary['max'], linestyle=':', color='gray', label='max')
-    axs[1].legend()
-    axs[1].set_ylabel('Latency (ms)')
 
-    df_cc_latency.plot.box(y='latency', ax=axs[1], grid=True)
-    axs[1].set_title('Causally Consistent Prototype EU')
 
-    fig.suptitle('Latency Distributions')
 
-    plt.tight_layout()    
-    plt.savefig(PATH + '/results/plots/latency_boxplot.png', dpi=300)
-    plt.clf()
-    plt.rcParams['figure.figsize'] = plt.rcParamsDefault['figure.figsize']
+# Univariate
+def get_latency_times(path, delay):
+    df = get_data(path + str(delay), 'readclient-' + LOCAL_REGION)
 
-def latency_stats(df_ev_latency, df_cc_latency): 
-    df_ev_stats = latency_table(df_ev_latency)
-    df_cc_stats = latency_table(df_cc_latency)
-    df_ev_stats.index = ['EV']
+    return df.groupby('id').apply(
+        lambda group: 
+            group.loc[group['logType'] == 'ROT_RESPONSE', 'time'].values[0] - 
+            group.loc[group['logType'] == 'ROT_REQUEST', 'time'].values[0]
+        ).reset_index(name='latency').tail(100).reset_index(drop=True)
+
+def latency_distribution_table(df_ev_latency, df_cc_latency, delay): 
+    df_ev_stats = df_describe(df_ev_latency, 'latency').round(2)
+    df_cc_stats = df_describe(df_cc_latency, 'latency').round(2)
+
+    df_ev_stats.index = ['EC']
     df_cc_stats.index = ['CC']
 
-    df_result = pd.concat([df_ev_stats.round(2), df_cc_stats.round(2)])
-    df_result['name'] = df_result.index
+    df_result = pd.concat([df_ev_stats, df_cc_stats])
+    df_result['consistency'] = df_result.index
     df_result = df_result.reset_index(drop=True)
 
-    df_result = pd.concat([df_result['name'], df_result.drop('name', axis=1)], axis=1)
+    df_result = pd.concat([df_result['consistency'], df_result.drop('consistency', axis=1)], axis=1)
 
     plt.table(cellText=df_result.values, colLabels=df_result.columns, cellLoc='center', loc='center')
     plt.axis('off')
     plt.annotate('Latency (ms)', (0.5, 0.7), xycoords='axes fraction', ha='center', va='bottom', fontsize=10)
-    plt.savefig(PATH + '/results/plots/latency_table.png', dpi=300, bbox_inches='tight')
+    plt.savefig(RESULT_PATH + '/latency_table_' + str(delay) + '.png', dpi=300, bbox_inches='tight')
     plt.clf()
-    plt.rcParams['figure.figsize'] = plt.rcParamsDefault['figure.figsize']
 
-def latency_histogram(df_ev_latency, df_cc_latency):
-    plt.figure(figsize=(10, 7))
-    sns.histplot(data=df_ev_latency, x="latency", kde=True, color="teal", label='EV')
-    sns.histplot(data=df_cc_latency, x="latency", kde=True, color="orange", label='CC')
-
-    plt.legend(loc='upper right')
-    plt.xlabel('Latency (ms)')
-    plt.ylabel('Frequency')
-
-    plt.savefig(PATH + '/results/plots/latency_histogram.png', dpi=300)
+def latency_boxplot(df, outliers = False, interval = 5, fig_size = (10, 10)):
+    _, ax = plt.subplots(figsize=fig_size)
+    sns.boxplot(data=df, x="latency", y="goodput", hue="consistency", showfliers=outliers)
+    ax.xaxis.grid(True)
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(base=interval))
+    ax.set_xlabel(ax.get_xlabel().capitalize(), labelpad=10)
+    ax.set_ylabel(ax.get_ylabel().capitalize(), labelpad=10)
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, [label.capitalize() for label in labels], loc="lower right")
+    plt.savefig(RESULT_PATH + '/latency_boxplot' + ('_outliers' if outliers else '') + '.png', dpi=300)
     plt.clf()
-    plt.rcParams['figure.figsize'] = plt.rcParamsDefault['figure.figsize']
 
-def latency_table(df):
-    return pd.DataFrame({
-        '99%': [df['latency'].quantile(q=0.99)],
-        '95%': [df['latency'].quantile(q=0.95)],
-        '70%': [df['latency'].quantile(q=0.70)],
-        '50%': [df['latency'].quantile(q=0.50)],
-        'mean': [df['latency'].mean()],
-        'max': [df['latency'].max()],
-        'min': [df['latency'].min()]})
+def latency_histogram(df):
+    g = sns.FacetGrid(df, col="goodput", hue="consistency", height=4, aspect=4, margin_titles=True)
+    g.map(sns.histplot, "latency", kde=True)
+    g.add_legend()
+    plt.savefig(RESULT_PATH + '/latency_histogram.png', dpi=300)
+    plt.clf()
 
+def latency_barplot(df):
+    _, ax = plt.subplots(figsize=(10, 10))
+    grouped_data = df.groupby(["goodput", "consistency"])["latency"]
+    average_latency = grouped_data.mean().reset_index()
+
+    sns.barplot(data=average_latency, x="goodput", y="latency", hue="consistency", width=0.8, edgecolor="#2a2a2a", linewidth=1.5)
+    ax.xaxis.grid(True)
+    ax.set_xlabel(ax.get_xlabel().capitalize(), labelpad=10)
+    ax.set_ylabel(ax.get_ylabel().capitalize(), labelpad=10)
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, [label.capitalize() for label in labels], loc="lower right")
+    plt.savefig(RESULT_PATH + '/latency_barplot.png', dpi=300)
+    plt.clf() 
 
 # Multivariate
 def latency_throughput_relation(path_ev, path_cc):
---
+++ evaluation/main.py
@@ -0,0 +1,15 @@
+import seaborn as sns
+import matplotlib as mpl
+from latency import latency_evaluation
+# from goodput import goodput_evaluation
+# from visibility import visibility_evaluation
+
+if __name__ == '__main__':
+    mpl.rcParams['font.family'] = 'Verdana'
+    sns.set(font_scale=1.5, style='whitegrid', context='notebook')
+    sns.set_palette("icefire")
+
+    latency_evaluation()
+    # goodput_evaluation()
+    # visibility_evaluation()
+    
