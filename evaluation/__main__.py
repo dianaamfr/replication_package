6bfe519f525c6fc5458e0aa7f24e300d22402ce2
Diana Freitas <diana.amfreitas@gmail.com>
2023-05-21 21:55:46 +0100

    Style latency plots

--- evaluation/__main__.py
+++ evaluation/__main__.py
@@ -1,14 +1,13 @@
 import seaborn as sns
-import matplotlib as mpl
+import matplotlib as plt
 import visualization
 
-mpl.rcParams['font.family'] = 'Verdana'
-sns.set(font_scale=1.5)
-sns.set_style("whitegrid", {'grid.linestyle': '--'})
+sns.set(style="whitegrid", font_scale=1.2, rc={"axes.edgecolor": "black", "axes.linewidth": 1})
 sns.set_palette(visualization.COLORS)
 
 visualization.single_client.latency_evaluation()
-visualization.single_client.goodput_evaluation()
-visualization.single_client.visibility_evaluation()
+# visualization.single_client.goodput_evaluation()
+# visualization.single_client.visibility_evaluation()
 
-visualization.multi_client.latency_evaluation()
+# visualization.multi_client.latency_evaluation()
+    
--- evaluation/visualization/single_client/latency.py
+++ evaluation/visualization/single_client/latency.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-from visualization.utils import PATH, CC_DIR, EC_DIR, LOCAL_REGION, DELAYS, GOODPUTS, LINESTYLES, MARKERS
+from visualization.utils import PATH, CC_DIR, EC_DIR, LOCAL_REGION, DELAYS, LINESTYLES, MARKERS
 from visualization.utils import get_data, df_describe
 
 EC_LATENCY_PATH = PATH + '/logs/latency' + EC_DIR + '/d_'
@@ -18,11 +18,11 @@ def latency_evaluation():
     for delay in DELAYS:
         df_ec_latency = get_latency_times(EC_LATENCY_PATH, delay)
         df_ec_latency['consistency'] = 'EC'
-        df_ec_latency['goodput'] = 1000//delay
+        df_ec_latency['delay'] = delay
 
         df_cc_latency = get_latency_times(CC_LATENCY_PATH, delay)
         df_cc_latency['consistency'] = 'CC'
-        df_cc_latency['goodput'] = 1000//delay
+        df_cc_latency['delay'] = delay
 
         dfs.append(df_ec_latency)
         dfs.append(df_cc_latency)
@@ -38,6 +38,7 @@ def latency_evaluation():
     latency_average_barplot(df)
 
     latency_throughput_relation(df)
+    latency_throughput_relation_errorbar(df)
 
 
 # Univariate
@@ -75,22 +76,20 @@ def latency_distribution_table(df_ec_latency, df_cc_latency, delay):
 
 
 def latency_boxplot(df, outliers=False, interval=5, fig_size=(10, 10)):
-    _, ax = plt.subplots(figsize=fig_size)
-    sns.boxplot(data=df, x="goodput", y="latency",
-                hue="consistency", showfliers=outliers, order=GOODPUTS)
-    ax.xaxis.grid(True)
-    ax.yaxis.set_major_locator(ticker.MultipleLocator(base=interval))
-    ax.set_xlabel(ax.get_xlabel().capitalize() + " (writes/s)", labelpad=10)
-    ax.set_ylabel(ax.get_ylabel().capitalize() + " (ms)", labelpad=10)
-    handles, labels = ax.get_legend_handles_labels()
-    ax.legend(handles, [label.capitalize()
-              for label in labels], loc="upper right")
-    plt.savefig(RESULT_PATH + '/latency_boxplot' +
-                ('_outliers' if outliers else '') + '.png', dpi=300)
+    plt.figure(figsize=fig_size)
+    sns.boxplot(data=df, x="delay", y="latency",
+                hue="consistency", hue_order=['EC','CC'], showfliers=outliers, order=DELAYS)
+    plt.gca().xaxis.grid(True)
+    plt.gca().yaxis.set_major_locator(ticker.MultipleLocator(base=interval))
+    plt.xlabel("Inter-Write Delay (writes/s)", labelpad=10)
+    plt.ylabel("Read Latency (ms)", labelpad=10)
+    handles, labels = plt.gca().get_legend_handles_labels()
+    plt.legend(handles, [label.capitalize() for label in labels], loc="upper right")
+    plt.savefig(RESULT_PATH + '/latency_boxplot' + ('_outliers' if outliers else '') + '.png', dpi=300)
     plt.clf()
 
 # def latency_histogram(df):
-#     g = sns.FacetGrid(df, row="goodput", hue="consistency", height=10, aspect=4, margin_titles=True)
+#     g = sns.FacetGrid(df, row="delay", hue="consistency", height=10, aspect=4, margin_titles=True)
 #     g.map(sns.histplot, "latency", kde=True)
 #     g.set(yscale='symlog')
 #     g.add_legend()
@@ -100,14 +99,14 @@ def latency_boxplot(df, outliers=False, interval=5, fig_size=(10, 10)):
 
 def latency_average_barplot(df):
     _, ax = plt.subplots(figsize=(10, 10))
-    grouped_data = df.groupby(["goodput", "consistency"])["latency"]
+    grouped_data = df.groupby(["delay", "consistency"])["latency"]
     average_latency = grouped_data.mean().reset_index()
 
-    sns.barplot(data=average_latency, x="goodput", y="latency",
-                hue="consistency", width=0.8, linewidth=1.5, order=GOODPUTS)
+    sns.barplot(data=average_latency, x="delay", y="latency",
+                hue="consistency", hue_order = ['EC','CC'], width=0.6, linewidth=1, edgecolor='black', order=DELAYS, alpha=0.9)
     ax.xaxis.grid(True)
-    ax.set_xlabel(ax.get_xlabel().capitalize() + " (writes/s)", labelpad=10)
-    ax.set_ylabel(ax.get_ylabel().capitalize() + " (ms)", labelpad=10)
+    ax.set_xlabel("Inter-Write Delay (writes/s)", labelpad=10)
+    ax.set_ylabel("Read Latency (ms)", labelpad=10)
     handles, labels = ax.get_legend_handles_labels()
     ax.legend(handles, [label.capitalize()
               for label in labels], loc="lower right")
@@ -122,19 +121,36 @@ def latency_throughput_relation(df):
     def estimator_99(data): return np.percentile(data, 99)
     def estimator_95(data): return np.percentile(data, 95)
 
-    sns.lineplot(data=df, x="goodput", y="latency", hue="consistency", style="consistency", markers=MARKERS, dashes=[LINESTYLES[0], LINESTYLES[0]],
-                 markersize=6, estimator=estimator_99, errorbar=None, linewidth=2, legend=False, markeredgewidth=1, markeredgecolor='w')
-    sns.lineplot(data=df, x="goodput", y="latency", hue="consistency", style="consistency", markers=MARKERS, dashes=[LINESTYLES[1], LINESTYLES[1]],
-                 markersize=8, estimator=estimator_95, errorbar=None, linewidth=2, legend=False, markeredgewidth=1, markeredgecolor='w')
-    sns.lineplot(data=df, x="goodput", y="latency", hue="consistency", style="consistency", markers=MARKERS, dashes=[LINESTYLES[2], LINESTYLES[2]],
-                 markersize=10, estimator=np.mean, errorbar=None, linewidth=2, legend=False, markeredgewidth=1, markeredgecolor='w')
+    sns.lineplot(data=df, x="delay", y="latency", hue="consistency", style="consistency", markers=MARKERS, dashes=[LINESTYLES[0], LINESTYLES[0]],
+                 markersize=8, estimator=estimator_99, errorbar=None, linewidth=3, markeredgewidth=1,
+                markeredgecolor='w', hue_order=['EC','CC'])
+    sns.lineplot(data=df, x="delay", y="latency", hue="consistency", style="consistency", markers=MARKERS, dashes=[LINESTYLES[1], LINESTYLES[1]],
+                 markersize=9, estimator=estimator_95, errorbar=None, linewidth=3, markeredgewidth=1,
+                markeredgecolor='w', hue_order=['EC','CC'])
+    sns.lineplot(data=df, x="delay", y="latency", hue="consistency", style="consistency", markers=MARKERS, dashes=[LINESTYLES[2], LINESTYLES[2]],
+                 markersize=10, estimator=np.mean, errorbar=None, linewidth=3, markeredgewidth=1, 
+                 markeredgecolor='w', hue_order=['EC','CC'])
 
     ax.xaxis.grid(True)
-    ax.set_xlabel(ax.get_xlabel().capitalize() + " (writes/s)", labelpad=10)
-    ax.set_ylabel(ax.get_ylabel().capitalize() + " (ms)", labelpad=10)
+    ax.set_xlabel("Inter-Write Delay (writes/s)", labelpad=10)
+    ax.set_ylabel("Read Latency (ms)", labelpad=10)
+    ax.xaxis.set_major_formatter(plt.FormatStrFormatter('%.0f'))
+    ax.legend(loc="upper right", labels=['EC P99', 'CC P99', 'EC P95', 'CC P95', 'EC Avg', 'CC Avg'])
+    plt.savefig(RESULT_PATH + '/latency_with_throughput.png', dpi=300)
+    plt.clf()
+
+def latency_throughput_relation_errorbar(df):
+    _, ax = plt.subplots(figsize=(8, 8))
+
+    sns.lineplot(data=df, x="delay", y="latency", hue="consistency", style="consistency", markers=MARKERS,
+                 markersize=10, linewidth=2, markeredgewidth=1, markeredgecolor='w', hue_order=['EC','CC'])
+
+    ax.xaxis.grid(True)
+    ax.set_xlabel("Inter-Write Delay (writes/s)", labelpad=10)
+    ax.set_ylabel("Average Read Latency (ms)", labelpad=10)
     ax.xaxis.set_major_formatter(plt.FormatStrFormatter('%.0f'))
     handles, labels = ax.get_legend_handles_labels()
     ax.legend(handles, [label.capitalize()
               for label in labels], loc="lower right")
-    plt.savefig(RESULT_PATH + '/latency_with_throughput.png', dpi=300)
+    plt.savefig(RESULT_PATH + '/latency_with_throughput_errorbar.png', dpi=300)
     plt.clf()
